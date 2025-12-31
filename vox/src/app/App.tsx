00f52e49aef195f7fe503cbe0c8de4ef931c9438
Ember <hello@loafykitten.cafe>
2025-12-31 04:10:53 -0600

    feat/chore: migrate to monorepo architecture
    swear-jar now incorporates 3 projects:
    - service: The bun application that serves the API and React widget
    - vox: The standalone bun application to utilize speech recognition and interface with the service API for automated swear detection
    - shared: Shared typings and functions across projects

---
+++ vox/src/app/App.tsx
@@ -0,0 +1,8 @@
+import { render } from 'ink'
+import Greeting from '../components/Greeting'
+
+const App = () => {
+	return <Greeting />
+}
+
+render(<App />)
---
+++ vox/src/components/Greeting.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'ink'
+
+export default function Greeting() {
+	return <Text>hewwo, world!</Text>
+}
